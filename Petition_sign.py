91dcc9c60693785e26084e33ce9f85615efc9823
Abdullah Maqsood <58801744+AbdullahMaqsood@users.noreply.github.com>
2020-10-19 17:56:40 -0400

    Signs A Single Petition

--- Petition_sign.py
+++ Petition_sign.py
@@ -2,16 +2,22 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://www.change.org/p/andy-beshear-justice-for-breonna-taylor?source_location=topic_page")
+petitions = ['https://www.change.org/p/justin-trudeau-canada-needs-to-step-up-for-mental-health?source_location=petitions_browse']
+
+for petition in petitions:
+    driver.get(petition)
 fname = 'abdullah'
 lname = 'maqsood'
 email = 'abdullahmaqsood2012@gmail.com'
 #signbutton = driver.find_element_by_xpath("/html/body/div[1]/main/div[2]/div[3]/div[2]/div/div/div/div[2]/div[2]/form/button[2]")
 signbutton = driver.find_element_by_css_selector('#page > div.container.xs-pan > div.row.mbxxl.xs-mbn.xs-mhn > div.hidden-xs > div > div > div > div.sc-AxheI.nPYYb > div.sc-AxheI.iDtiqJ > form > button.btn.btn-big.btn-full.btn-action')
+fname_box = driver.find_element_by_css_selector('#firstName')
+lname_box = driver.find_element_by_css_selector('#lastName')
+email_box = driver.find_element_by_css_selector('#email')
 print(signbutton)
 publiccheck = driver.find_element_by_name("public")
-#fnamebox.send_keys(fname)
-#lnamebox.send_keys(lname)
-#emailbox.send_keys(email)
+fname_box.send_keys(fname)
+lname_box.send_keys(lname)
+email_box.send_keys(email)
 #publiccheck.click()
 signbutton.click()
